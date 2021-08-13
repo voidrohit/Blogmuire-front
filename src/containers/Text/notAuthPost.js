1138dc0149e782f290fa1ac054eba856e6f6289a
Rohit Kumar Singh <55323686+voidrohit@users.noreply.github.com>
2021-08-13 09:59:00 +0530

    Add files via upload

---
+++ src/containers/Text/notAuthPost.js
@@ -0,0 +1,23 @@
+import React,{ useState , useEffect}from 'react';
+import axios from 'axios';
+
+const NotAuthPost = () => {
+
+    const [post,setPost] = useState([]);
+    let id = window.location.pathname
+
+    useEffect(()=>{
+        const fetchPost = async ()=>{
+        const res = await axios.get("/posts"+id)
+        setPost(res.data);
+        }
+        fetchPost();
+    });
+    return (
+        <div style={{ display: "flex", flexDirection: "column"}}>
+            <div id="text" className="text" style={{display: "block", margin: "auto"}} dangerouslySetInnerHTML={{__html: post.text}} />
+        </div>
+    )
+}
+
+export default NotAuthPost;
