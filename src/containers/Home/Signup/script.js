f1a632d2f1c18e30ae9acaf74232487aa1f25148
Rohit Kumar Singh <55323686+voidrohit@users.noreply.github.com>
2021-08-13 09:51:23 +0530

    Add files via upload

---
+++ src/containers/Home/Signup/script.js
@@ -0,0 +1,11 @@
+const signUpButton = document.getElementsByClassName('ghost');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
